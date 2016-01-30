a73dd379678d0072ee65df638796c09387c20c25
jparent <jeanp.parent@gmail.com>
2016-01-30 04:58:39 -0500

    add basic splashscreen with skippable settings

---
+++ Assets/scripts/MenuInteractions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInteractions : MonoBehaviour
+{
+    public int nextLevel;
+    public bool isSkippable = true;
+    public bool hasAutoSkips = false;
+    public int timeBeforeSkip = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSkippable)
+        {
+            if (Input.anyKeyDown)
+            {
+                Application.LoadLevel(nextLevel);
+            }
+        }
+    }
+
+    void Start()
+    {   
+        // this won't lead anywhere... let's fix it
+        if(!isSkippable && !hasAutoSkips)
+        {
+            isSkippable = true;
+            hasAutoSkips = true;
+        }
+
+        if (hasAutoSkips)
+        {
+            StartCoroutine(AutoSkip());
+        }
+    }
+
+    IEnumerator AutoSkip()
+    {        
+        yield return new WaitForSeconds(timeBeforeSkip);
+        Application.LoadLevel(nextLevel);
+
+    }
+}
