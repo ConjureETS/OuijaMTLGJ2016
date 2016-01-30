27fbca1c4632b78329f77b327d16b3be32e1ee17
Dmitri Kolytchev <kolytchev.d@gmail.com>
2016-01-30 01:15:35 -0500

    initial commit

---
+++ Assets/scripts/PlayerBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public int playerNumber;
+	public float speed = 10;
+	public GameObject anchor;
+	public float maxRangeFromAnchor = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string horizAxisString = "Horizontal"+(playerNumber==1?"":playerNumber.ToString());
+		string vertAxisString = "Vertical"+(playerNumber==1?"":playerNumber.ToString());
+
+		Vector3 movement = new Vector3 (Input.GetAxis(horizAxisString), Input.GetAxis(vertAxisString), 0) * speed;
+
+		if (movement.magnitude > speed)
+			movement = movement.normalized * speed;
+
+		//if((transform.position + movement - anchor.transform.position).magnitude >= maxRangeFromAnchor)
+		//	movement -= anchor.transform.position - transform.position
+
+		transform.position += movement * Time.deltaTime;
+	}
+}
---
+++ Assets/scripts/SelectorBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectorBehaviour : MonoBehaviour {
+
+	public GameObject[] follows;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 averagePosition = new Vector3 (0, 0, 0);
+		for (int i = 0, j = follows.Length; i<j; i++) {
+			averagePosition += follows[i].transform.position / j;
+		}
+		transform.position = averagePosition;
+	}
+}
