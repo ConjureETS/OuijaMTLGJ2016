7daedf9b55f2b9293bfa0d7a2d31a62779a37a7b
RosimInc <rosiminc7@gmail.com>
2016-01-30 16:40:16 -0500

    Added glow when walking onto tiles.

--- Assets/scripts/RuneBehaviour.cs
+++ Assets/scripts/RuneBehaviour.cs
@@ -6,6 +6,8 @@ public class RuneBehaviour : MonoBehaviour {
 	public static float range;
 	public SpriteRenderer symbol;
 	public int letterNum;
+	private float lightState = 0f;
+	public const float LightTime = 1.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +16,27 @@ public class RuneBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (lightState > 0f)
+		{
+			lightState -= Time.deltaTime / LightTime;
+			if (lightState < 0)
+				lightState = 0;
+			//symbol.color = new Color(1 - lightState, 1f, 1 - lightState);
+
+			symbol.color = new Color(1f - lightState * 0.5f, 1f - lightState * 0.8f, 1f);
+
+
+			//symbol.color = new Color(1f - lightState * 0.4f, 1f - lightState * 0.4f, 1f);
+		}
+
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			lightState = 1f;
+		}
 	}
 
 	public void SetSymbol(int letterNum)
