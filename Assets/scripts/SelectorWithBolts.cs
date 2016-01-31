f436e8f341ef0daa265c4651f91e35df245b34b0
Patrice Vignola <vignola.patrice@gmail.com>
2016-01-30 22:22:33 -0500

    Add the dash colors to the ropes

--- Assets/scripts/SelectorWithBolts.cs
+++ Assets/scripts/SelectorWithBolts.cs
@@ -6,12 +6,32 @@ public class SelectorWithBolts : MonoBehaviour
 {
     public Transform[] Bolts;
     public Transform[] RootCylinders;
+    public GameObject[] Ropes;
+    public Color[] DashColors;
+
+    private const float DASH_COOLDOWN = 6f;
 
     private Rigidbody rb;
+    private MeshRenderer[][] playerCylinders;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        playerCylinders = new MeshRenderer[Ropes.Length][];
+
+        for (int i = 0; i < Ropes.Length; i++)
+        {
+            playerCylinders[i] = Ropes[i].GetComponent<Transform>().GetComponentsInChildren<MeshRenderer>();
+        }
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < Ropes.Length; i++)
+        {
+            ReplenishPlayerDashMeter(i);
+        }
     }
 
     void Update()
@@ -23,8 +43,36 @@ public class SelectorWithBolts : MonoBehaviour
 		}
     }
 
-    public void ApplyForce(int playerID, Vector3 force)
+    public void ReplenishPlayerDashMeter(int playerId)
     {
-        rb.AddForceAtPosition(force, Bolts[playerID].position);
+        ResetRope(playerId);
+
+        StartCoroutine(ReplenishPlayerDashMeterCoroutine(playerId));
+    }
+
+    private void ResetRope(int playerId)
+    {
+        foreach (MeshRenderer renderer in playerCylinders[playerId])
+        {
+            renderer.material.color = Color.gray;
+        }
+    }
+
+    private IEnumerator ReplenishPlayerDashMeterCoroutine(int playerId)
+    {
+        float elapsedTime = 0f;
+
+        int cylindersCount = playerCylinders[playerId].Length;
+
+        while (elapsedTime < DASH_COOLDOWN)
+        {
+            
+            elapsedTime += Time.deltaTime;
+            int cylinderIndex = (int)(elapsedTime * cylindersCount / DASH_COOLDOWN - 1);
+            Debug.Log(cylinderIndex);
+            playerCylinders[playerId][cylinderIndex].material.color = DashColors[playerId];
+
+            yield return null;
+        }
     }
 }
